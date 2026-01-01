3ee122979d82d5f29322b25bbdaf3043f4e8c71a
Tala AL-Khateeb <talaalkhateeb04@gmail.com>
2026-01-01 17:59:14 +0300

    Delete core directory
    لانه غير متناسق مع مجلد المشروع

--- core/Urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.UserListCreate.as_view(), name='user-list'),
-    path('payments/', views.PaymentListCreate.as_view(), name='payment-list'),
-    path('ratings/', views.RatingListCreate.as_view(), name='rating-list'),
-    path('ratings/<int:pk>/', views.RatingDetail.as_view(), name='rating-detail'),
-]
